605b54d3613c54d0780da8cfcdd2d7fbf49aa722
mararo-ops <danielmararo1@gmail.com>
2023-12-11 12:48:09 +0300

    initializing faker

--- lib/seed.py
+++ lib/seed.py
@@ -2,3 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Restaurant, Customer, Review
 from faker import Faker
+
+# Initialize Faker
+fake = Faker()
