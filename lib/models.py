20075a0ba2215a368be50722dadccb5d47e5fc98
mararo-ops <danielmararo1@gmail.com>
2023-12-11 11:48:49 +0300

    customer updates

--- lib/models.py
+++ lib/models.py
@@ -2,3 +2,17 @@ from sqlalchemy import create_engine, func
 from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, Table
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:///restaurants.db')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+restaurant_customer = Table(
+    'restaurant_customers',
+    Base.metadata,
+    Column('restaurant_id', ForeignKey('restaurants.id'), primary_key=True),
+    Column('customer_id', ForeignKey('customers.id'), primary_key=True),
+    extend_existing=True,
+)
