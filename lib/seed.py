0f8066146417b51fa9517af44dba12b4142ba441
mararo-ops <danielmararo1@gmail.com>
2023-12-11 12:49:28 +0300

    Create the database engine and bind it to your SQLAlchemy models

--- lib/seed.py
+++ lib/seed.py
@@ -5,3 +5,28 @@ from faker import Faker
 
 # Initialize Faker
 fake = Faker()
+
+# Create the database engine and bind it to your SQLAlchemy models
+engine = create_engine('sqlite:///restaurants.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def create_fake_restaurant():
+    return Restaurant(
+        name=fake.company(),
+        price=fake.random_int(min=1, max=5)
+    )
+
+def create_fake_customer():
+    return Customer(
+        full_name=fake.name()
+    )
+
+def create_fake_review(restaurant, customer):
+    return Review(
+        star_rating=fake.random_int(min=1, max=5),
+        restaurant=restaurant,
+        customer=customer
+    )
+
