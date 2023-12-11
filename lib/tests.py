9e4ffe847ea82ecd3bce128455e039f67b99f1fc
mararo-ops <danielmararo1@gmail.com>
2023-12-11 12:52:40 +0300

    addd the data to the session and commmit changes

--- lib/tests.py
+++ lib/tests.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
 
     customer1 = Customer(full_name='John Doe' , restaurants= [restaurant1, restaurant2])
     customer2 = Customer(full_name='Jane Smith', restaurants = [restaurant1,restaurant2])
+
+
+# Add the data to the session and commit the changes
+    session.add_all([restaurant1, restaurant2, customer1, customer2])
+    session.commit()
+
+    
+
+    review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
+    review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer1)
+    review3 = Review(star_rating=3, restaurant=restaurant1, customer=customer2)
